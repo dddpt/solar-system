d1a3e83d20507e183e8310d252070a55f79b1100
Didier <didier.dupertuis@unil.ch>
2020-05-15 20:17:00 +0200

    put a demo for a descriptive kepler orbital system

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,13 @@ const path = require("path");
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, "src/js/main.js"),
+  entry: {
+    "dist/js/bundle.js": path.resolve(__dirname, "src/js/main.js"),
+    "demo/kepler/dist/js/bundle.js": path.resolve(__dirname, "demo/kepler/src/js/main.js")
+  },
   output: {
-    filename: "js/bundle.js"
+    path: path.resolve(__dirname,"."),
+    filename: "[name]"
   },
   module: {
     rules: [
